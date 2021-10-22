d21d4eadd4cfdfee48b1d7c7c78a17657358d1d9
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-10-22 23:35:56 +1100

    updt: added initial codes per class and added require fs

---
+++ index.js
@@ -0,0 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const Employee = require("./lib/Employee");
+
+// const employee = new Employee();
+
+console.log("Lets build our team.\n");
+// employee.();
