9bd9ad692738f21654b1ca6d95842017ab5ff546
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-10-23 00:31:04 +1100

    updt: updated code in Manager class and index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const Employee = require("./lib/Employee");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
 
-const arr_engineerTeam = [];
+const engineeringTeam = [];
 
-// const employee = new Employee();
+const employee = new Employee();
 
-console.log("Lets build our team.\n");
-// employee.();
+employee.welcome();
+employee.addManager();
