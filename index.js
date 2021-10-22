65f37988adc394eb5a6627360d31c174d3ff245c
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-10-22 23:41:37 +1100

    updt: added classes and team array

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const arr_engineerTeam = [];
 
 // const employee = new Employee();
 
