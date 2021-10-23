e8658f0671ecd22334ddbd427392f671cc96026e
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-10-24 06:21:52 +1100

    feat: able to generate all member cards on html

--- src/Template.js
+++ src/Template.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { forEach } = require('lodash');
 
 class createHTML{
     constructor(response) {
@@ -6,31 +7,91 @@ class createHTML{
     }
 
     createPage() {
-        const generateHTML = () => `
-        <!DOCTYPE html>
+
+        const res = this.response;
+
+        const arr_htmlElements = [];
+
+        const topEl = 
+        `<!DOCTYPE html>
         <html lang="en">
             <head>
                 <meta charset="UTF-8">
                 <meta http-equiv="X-UA-Compatible" content="ie=edge">
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-                <title>Team Profile</title>
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
+                <title>${res[0]}</title>
             </head>
             <body>
                 <header class="container-fluid jumbotron-custom bg-dark text-white">
-                    <h1 class="display-4 text-center">${this.response[0]}</h1>
+                    <h1 class="display-4 text-center">${res[0]}</h1>
                 </header>
 
-                <section></section>
-                </div>
-            </div>
-            </body>
-        </html>
-        `;
+                <div class="container-fluid d-flex">`;
 
-        console.log(this.response);
-        const htmlPageContent = generateHTML(this.response);
+                arr_htmlElements.push(topEl);
 
-        fs.writeFile('./dist/index.html', htmlPageContent, (err) =>
+                let new_res = [];
+
+                res.slice(1).forEach(data => 
+                    new_res.push(data)
+                    );
+
+                new_res.forEach(member => 
+                    {
+                        if (member.role === "Manager") {
+                            const memberCards = 
+                            `<div class="col-4 mt-4">
+                                <div class="card h-100 mx-4 px-4">
+                                    <h3>${member.name}</h3>
+                                    <h4>☕ ${member.role}</h4>
+                                    <div class="card-body">
+                                    <p class="id">ID: ${member.id}</p>
+                                    <p class="email">Email: <a href="mailto:${member.email}">${member.email}</a></p>
+                                    <p class="office">Office Number: ${member.officePhone}</p>
+                                    </div>
+                                </div>
+                        </div>`;
+                        arr_htmlElements.push(memberCards);
+                        }
+
+                        if (member.role === "Engineer") {
+                            const memberCards = 
+                            `<div class="col-4 mt-4">
+                                <div class="card h-100 mx-4 px-4">
+                                    <h3>${member.name}</h3>
+                                    <h4>☕ ${member.role}</h4>
+                                    <div class="card-body">
+                                    <p class="id">ID: ${member.id}</p>
+                                    <p class="email">Email: <a href="mailto:${member.email}">${member.email}</a></p>
+                                    <p class="office">Github : ${member.github}</p>
+                                    </div>
+                                </div>
+                        </div>`;
+                        arr_htmlElements.push(memberCards);
+                        }
+
+                        if (member.role === "Intern") {
+                            const memberCards = 
+                            `<div class="col-4 mt-4">
+                                <div class="card h-100 mx-4 px-4">
+                                    <h3>${member.name}</h3>
+                                    <h4>☕ ${member.role}</h4>
+                                    <div class="card-body">
+                                    <p class="id">ID: ${member.id}</p>
+                                    <p class="email">Email: <a href="mailto:${member.email}">${member.email}</a></p>
+                                    <p class="office">School : ${member.school}</p>
+                                    </div>
+                                </div>
+                        </div>`;
+                        arr_htmlElements.push(memberCards);
+                        }
+                    });
+
+                const btmEl = `</div></body></html>`;
+                arr_htmlElements.push(btmEl);
+
+        fs.writeFile('./dist/index.html', arr_htmlElements.join(""), (err) =>
             err ? console.log(err) : console.log('Successfully created index.html!')
         );
     }
