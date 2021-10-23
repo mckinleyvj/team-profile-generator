7dc27e1551b0a12ad8e0d5212becb7c4e3f8392d
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-10-23 23:32:05 +1100

    updt: added welcome and getManager prompts

--- index.js
+++ index.js
@@ -4,11 +4,57 @@ const fs = require("fs");
 const Employee = require("./lib/Employee");
 // const Engineer = require("./lib/Engineer");
 // const Intern = require("./lib/Intern");
-// const Manager = require("./lib/Manager");
+const Manager = require("./lib/Manager");
 
 const engineeringTeam = [];
 
-const employee = new Employee();
+const welcome = () => {
+    console.log(`-----------------------------------------`);
+    console.log(`           Team Profile Builder          `);
+    console.log(`=========================================`);
+        
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Team name :",
+            name: "teamName",
+        }
+    ])
+    .then(function(response){
+        engineeringTeam.push(response.teamName);
+        console.log(engineeringTeam);
+    })
+};
 
-employee.welcome();
-employee.addManager();
+const getManager = () => {
+    return inquirer.prompt ([
+        {
+            type: "input",
+            message: "Enter team manager's name :", 
+            name: "mngr_name",
+        },
+        {
+            type: "input",
+            message: "Enter the manager's email :",
+            name: "mngr_email",
+        },
+        {
+            type: "input",
+            message: "Enter the manager's office number :",
+            name: "off_number",
+        }
+    ])
+    .then(user_input => {
+        const { mngr_name, mngr_email, off_number } = user_input;
+        const manager = new Manager(mngr_name, 1, mngr_email, off_number);
+
+        engineeringTeam.push(manager);
+        console.log(engineeringTeam);
+    })
+};
+
+welcome()
+    .then(getManager)
+    .catch() = err => {
+        console.log(err);
+    };
