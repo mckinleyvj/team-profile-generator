72e36b1e945e7303b3351217ccfd7441bb3bca73
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-10-24 21:02:37 +1100

    test: added Engineer object test and fixed tests

--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,15 +1,26 @@
 const Engineer = require('../lib/Engineer');
 
-describe("getGitHub", () => {
-    it("should return this.github as received from input", () => {
+describe("creating the Engineer Object", () => {
+    it("should return without error as per user input", () => {
         const engineer = new Engineer("Mckinley", 1, 'mckinley@gg.com', 'mckinleyvj');
 
-        expect(engineer.github).toEqual("mckinleyvj");
+        expect(engineer.name).toEqual(expect.any(String));
+        expect(engineer.id).toEqual(expect.any(Number));
+        expect(engineer.email).toEqual(expect.any(String));
+        expect(engineer.github).toEqual(expect.any(String));
+    });
+  });
+
+describe("getGitHub", () => {
+    it("should return github username without error as per user input", () => {
+        const engineer = new Engineer("Mckinley", 1, 'mckinley@gg.com', 'mckinleyvj');
+
+        expect(engineer.github).toEqual(expect.any(String));
     });
   });
 
   describe("getRole", () => {
-    it("should return this.role as received from input", () => {
+    it("should return should return engineer as role without error as per class without error as per user input", () => {
         const engineer = new Engineer("Mckinley", 1, 'mckinley@gg.com', 'mckinleyvj');
 
         expect(engineer.role).toEqual("Engineer");
