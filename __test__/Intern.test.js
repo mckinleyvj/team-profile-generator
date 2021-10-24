9cf9ab408456f2b558660f10b3e16d0fed372a6f
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-10-24 21:05:26 +1100

    test: added Intern object test and fixes

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -1,15 +1,26 @@
 const Intern = require('../lib/Intern');
 
-describe("getSchool", () => {
-    it("should return this.school as received from input", () => {
+describe("creating the Intern Object", () => {
+    it("should return without error as per user input", () => {
         const intern = new Intern("Mckinley", 1, 'mckinley@gg.com', 'Deakin University');
 
-        expect(intern.school).toEqual("Deakin University");
+        expect(intern.name).toEqual(expect.any(String));
+        expect(intern.id).toEqual(expect.any(Number));
+        expect(intern.email).toEqual(expect.any(String));
+        expect(intern.school).toEqual(expect.any(String));
+    });
+  });
+
+describe("getSchool", () => {
+    it("should return the school name without error as per user input", () => {
+        const intern = new Intern("Mckinley", 1, 'mckinley@gg.com', 'Deakin University');
+
+        expect(intern.school).toEqual(expect.any(String));
     });
   });
 
   describe("getRole", () => {
-    it("should return this.role as received from input", () => {
+    it("should return should return intern as role without error as per class without error as per user input", () => {
         const intern = new Intern("Mckinley", 1, 'mckinley@gg.com', 'Deakin University');
 
         expect(intern.role).toEqual("Intern");
