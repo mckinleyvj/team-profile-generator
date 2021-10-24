a81008f49eecd9cf0cb973db47be2038a7024ebd
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-10-24 21:07:22 +1100

    test: added manager object test and fixes

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,7 +1,18 @@
 const Manager = require('../lib/Manager');
 
+describe("creating the Manager Object", () => {
+    it("should return without error as per user input", () => {
+        const manager = new Manager("Mckinley", 1, 'mckinley@gg.com', '041555555');
+
+        expect(manager.name).toEqual(expect.any(String));
+        expect(manager.id).toEqual(expect.any(Number));
+        expect(manager.email).toEqual(expect.any(String));
+        expect(manager.officePhone).toEqual(expect.any(Number));
+    });
+  });
+
   describe("getRole", () => {
-    it("should return this.role as received from input", () => {
+    it("should return should return engineer as role without error as per class without error as per user input", () => {
         const manager = new Manager("Mckinley", 1, 'mckinley@gg.com', '041555555');
 
         expect(manager.role).toEqual("Manager");
